2316d9af2be180c577076ebef1bf7c667a1a3f30
Andrew Sieg <adsieg01@gmail.com>
2020-07-14 15:31:47 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-#import pymongo
-#client = pymongo.MongoClient(
-   #"mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/test?retryWrites=true&w=majority")
-#db = client.test
+import pymongo
+client = pymongo.MongoClient(
+   "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
