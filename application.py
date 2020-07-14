a56e0023b84c310c6178f7f23275e4d6f07181be
Andrew Sieg <adsieg01@gmail.com>
2020-07-14 14:59:09 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,6 @@
 from flask import Flask
-import pymongo
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
-   
-   # client = pymongo.MongoClient(
-   # "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/test?retryWrites=true&w=majority")
-# db = client.test
