29630fd8a3c4942a3402bc8344df6782b096cccc
Andrew Sieg <adsieg01@gmail.com>
2020-07-15 14:46:31 -0400

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import dns
 app = Flask(__name__)
 
 client = pymongo.MongoClient(
-   "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
+   "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/?retryWrites=true&w=majority")
 db = client.["sample_airbnb"]
 mycol = db["collection-test-creation"]
 
