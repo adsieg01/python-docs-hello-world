811590ed7aae4e83f19898d6fcf81b7d62308f99
Andrew Sieg <adsieg01@gmail.com>
2020-07-15 14:16:55 -0400

    Connecting to Sample Airbnb

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ import dns
 app = Flask(__name__)
 
 client = pymongo.MongoClient(
-   "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/test?retryWrites=true&w=majority")
-db = client.test
+   "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
+db = client.sample_airbnb
 
 @app.route("/")
 def hello():
