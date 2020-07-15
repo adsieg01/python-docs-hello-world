abb6e51f9304e238fffa0d8352c9917aaa33105a
Andrew Sieg <adsieg01@gmail.com>
2020-07-15 14:45:19 -0400

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient(
    "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/sample_airbnb?retryWrites=true&w=majority")
-db = client.sample_airbnb
+db = client.["sample_airbnb"]
 mycol = db["collection-test-creation"]
 
 @app.route("/")
