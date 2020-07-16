fc5269afc0194dec659761c79a73b444876c1ad2
Andrew Sieg <adsieg01@gmail.com>
2020-07-16 12:18:31 -0400

    npe-connection-via-hello-world

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 client = pymongo.MongoClient(
    "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/?retryWrites=true&w=majority")
 db = client["sample_airbnb"]
-mycol = db["ip-test-no-virt-ip"]
+mycol = db["npe-connection-via-hello-world"]
 
 #a document
 tester = { "name": "Saranya", "address": "Kochi" }
@@ -17,5 +17,5 @@ x = mycol.insert_one(tester)
 
 @app.route("/")
 def hello():
-    return "Testing Mongo Connection to sample database"
+    return "npe-connection-via-hello-world"
    
