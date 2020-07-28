e4dd21c534e082aac0eea1c97331195b0ee415fe
Andrew Sieg <ASieg@humana.com>
2020-07-28 14:47:03 -0400

    updated client

--- application.py
+++ application.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient(
    "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/?retryWrites=true&w=majority")
-db = client["sample_airbnb"]
-mycol = db["npe-connection-via-hello-world-both-ips"]
+db = client["sample_db"]
+mycol = db["test-collection"]
 
 #a document
 tester = { "name": "Test", "address": "Document" }
