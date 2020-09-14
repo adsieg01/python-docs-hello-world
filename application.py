8a7c9fde1afd742216b326374990c8bb3c8bf1e1
Andrew Sieg <adsieg01@gmail.com>
2020-09-14 12:18:23 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ import dns
 
 app = Flask(__name__)
 
+mongo "mongodb+srv://cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/test" --username Dummy_User_01
+Password: xEXGTNByQzEBDyKF
+
 @app.route("/")
 def hello():
     return "This is a test, if you can read this app service is up"
