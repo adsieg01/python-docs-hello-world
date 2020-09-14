63ae04e1225bcdfa3d28891feaf6f7aeec3bd922
Andrew Sieg <adsieg01@gmail.com>
2020-09-14 13:38:10 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
 from flask import Flask
 import pymongo
 import dns
-
 app = Flask(__name__)
 
+client = pymongo.MongoClient(
+   "mongodb+srv://Dummy_User_01:xEXGTNByQzEBDyKF@cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/?retryWrites=true&w=majority")
+db = client["sample_db"]
+mycol = db["test-collection"]
+
+#a document
+tester = { "name": "Test", "address": "Document" }
+#insert a document to the collection
+x = mycol.insert_one(tester)
 @app.route("/")
 def hello():
     return "This is a test, if you can read this app service is up"
-   
-mongo "mongodb+srv://cdf-sbx-azure-atlas-1.sn9jp.azure.mongodb.net/test" --username Dummy_User_01
-Password: xEXGTNByQzEBDyKF
